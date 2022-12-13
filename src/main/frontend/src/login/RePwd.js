e27882a91313fe45e4447be49817ec0510a363d5
sb9479 <sb9479@naver.com>
2022-12-13 17:51:37 +0900

    수빈/12.13/pw

--- src/main/frontend/src/login/RePwd.js
+++ src/main/frontend/src/login/RePwd.js
@@ -5,20 +5,13 @@ import Modal from "../util/Modal";
 
 const RePwd = () => {
     // 로그인된 아이디 불러오기
-    const localId =window.localStorage.getItem("userId");
+    let localId = window.localStorage.getItem("userId");
 
     const [inputId, setInputId] = useState("");
-    const [inputMail, setInputMail] = useState("");
-
-    const [mailMsg, setMailMsg] = useState("");
-    const [isMail, setIsMail] = useState(false);
-
-    // 아이디와 이메일이 맞으면 새로운 비밀번호를 지정할 수 있는 html을 불러옴
-    const [isNewPwd, setIsNewPwd] = useState(false);
 
     //새로 설정한 비밀번호가 정규식이 맞는지, 비밀번호 중복 체크
+    const [isRePwd, setIsRePwd] = useState(false);
     const [isRePwdCheck, setIsRePwdCheck] = useState(false);
-    const [isRePwdConCheck, setIsRePwdConCheck] = useState(false);
 
     //새로운 비밀번호/비밀번호 확인 값
     const [newPwd, setNewPwd] = useState("");
@@ -34,6 +27,11 @@ const RePwd = () => {
         setModalOpen(false);
     };
 
+    // 아이디
+    const onChangeId = (e) => {
+        setInputId(e.target.value)
+    };
+
     // 비밀번호 입력
     const onChangePwd = (e) => {
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/
@@ -41,14 +39,14 @@ const RePwd = () => {
         setNewPwd(passwordCurrent);
         if(!passwordRegex.test(passwordCurrent)) {
             setPwdMsg("숫자+영문자+특수문자를 포함하여 8자 이상 입력해주세요.")
-            setIsRePwdCheck(false)
+            setIsRePwd(false)
         } else {
             setPwdMsg("안전한 비밀번호입니다 :D")
-            setIsRePwdCheck(true);
+            setIsRePwd(true);
         }
         if(passwordCurrent !== newPwdChe) {
             setConPwdMsg("비밀번호가 일치하지 않습니다.")
-            setIsRePwdConCheck(false)
+            setIsRePwdCheck(false)
         }
     }
 
@@ -58,29 +56,10 @@ const RePwd = () => {
         setNewPwdChe(passwordCurrent)
         if (passwordCurrent !== newPwd) {
             setConPwdMsg("비밀번호가 일치하지 않습니다.")
-            setIsRePwdConCheck(false)
+            setIsRePwdCheck(false)
         } else {
             setConPwdMsg("비밀번호가 일치합니다.")
-            setIsRePwdConCheck(true);
-        }
-    }
-
-    // api 호출
-    const onClickFindPwd = async() => {
-        const res = await MuteApi.researchPwd(inputId, inputMail, "Type_PWD");
-        //console.log(res.data);
-        if(res.data) {
-            setModalOpen(true);
-            setComment("비밀번호를 재설정합니다.")
-            setIsNewPwd(true);
-        } else {
-            setModalOpen(true);
-            setComment("가입된 정보가 없습니다.")
-        }
-    }
-    const onKeyDownFindPwd = (e) => {
-        if(e.key === 'Enter'){
-            onClickFindPwd();
+            setIsRePwdCheck(true);
         }
     }
 
@@ -91,7 +70,7 @@ const RePwd = () => {
             console.log(res.data);
 
             if(res.data) {
-                setIsNewPwd(false);
+                //setIsNewPwd(false                                                                                               );
                 setModalOpen(true);
                 setComment("비밀번호 재설정이 완료되었습니다. 로그인해주세요")
             } else {
@@ -107,7 +86,6 @@ const RePwd = () => {
     }
 
     return(
-
         <>
             <h5>비밀번호 재설정</h5>
             {/* 아이디 불러오기 */}
@@ -121,7 +99,7 @@ const RePwd = () => {
             <p> {newPwdChe.length > 0 && <span>{conPwdMsg}</span>}<br/>
                 <input type="password" placeholder="비밀번호 확인" onChange={onChangePwdCheck} value={newPwdChe} onKeyDown={onKeyDownRePwd}></input>
             </p>
-            <button disabled={!(isRePwdCheck && isRePwdConCheck)} onClick={()=>onClickRePwd()}>재설정</button>
+            <button disabled={!(newPwd && newPwdChe)} onClick={()=>onClickRePwd()}>재설정</button>
 
             <div>
                 {/* 다른 페이지 연결 */}
