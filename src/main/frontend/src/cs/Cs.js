be1ea86aa5a93d700026dec9cdbcf2a0730abcc3
sb9479 <sb9479@naver.com>
2022-12-15 15:24:33 +0900

    수빈/12.15/yarn.lock

--- src/main/frontend/src/cs/Cs.js
+++ src/main/frontend/src/cs/Cs.js
@@ -1,9 +1,29 @@
+import {useState} from "react";
+import styled from "styled-components";
+
+const Answer = styled.div`
+    display: none;
+    padding-bottom: 30px;
+    .faq-title {
+        border: 1px solid #e0e0e0;
+    }
+`;
+
+const Cs = ({title, content}) => {
+    const items = document.querySelectorAll('.question');
+
+    function openCliseAnswer() {
+        const answerId = this.id.replace('que', 'ans');
+    }
 
 
-const Cs = () => {
+
+
+
 return(
     <>
-    <h3>고객센터 페이지입니다</h3>
+    <h3>Mute 고객센터입니다</h3>
+
     </>
 )
 }
