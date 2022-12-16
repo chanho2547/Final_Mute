c3a6613e2c6942e6edfc49e1d85ed673f5b40968
CHAN-HO <chanho2547@gmail.com>
2022-12-16 16:35:17 +0900

    은종/12.16/musicalDetail

--- src/main/frontend/src/musical/MusicalDetail.js
+++ src/main/frontend/src/musical/MusicalDetail.js
@@ -18,7 +18,7 @@ const MusicalDetail = (props) => {
     // 찜하기
     const [wish, setWish] = useState(false);
     const [modalWishReg, setModalWishReg] = useState(false); // 찜 등록했을 경우
-    const [modalWishCancel, setModalWishCancel] = useState(false); // 찜 취소했을 경우
+    const [modalWishCancel, setModalWishCancel] = useState(false); // 찜 취소했을 때 
     const [modalNotLogin, setModalNotLogin] = useState(false); // 로그인 안했을 경우
 
     // 관심상품 등록 모달
@@ -26,9 +26,15 @@ const MusicalDetail = (props) => {
         setModalWishReg(false); // 확인버튼 => 찜 등록 끝
     }
 
-    // 관심상품 취소 모달
-    const closeModalWishCancel = () => {
+    // 관심상품 취소 모달 NO 선택
+    const closeModalWishCancelN = () => {
         setModalWishCancel(false); // yes버튼, no버튼 => yes버튼 클릭시 찜하기 취소
+    }
+
+    // 관심상품 취소 모달 YES 선택
+    const closeModalWishCancelY = () => {
+        setModalWishCancel(false); // yes버튼, no버튼 => yes버튼 클릭시 찜하기 취소
+        wish(false);
     }
 
     // 로그인 필요 서비스 모달
@@ -38,9 +44,10 @@ const MusicalDetail = (props) => {
     }
 
     // 지금 필요한 것
-    // 1. 로그인 안하고 찜 누르면 로그인 화면으로 이동
-    // 2. 찜하면 찜 완료 모달창
-    // 3. 이미 찜하면 찜 취소 모달창 + 기능
+    // 1. 로그인 안하고 찜 누르면 로그인 화면으로 이동 => 완
+    // 2. 찜하면 찜 완료 모달창 => 완
+    // 3. 이미 찜하면 찜 취소 모달창 여기서 yes누르면 모달창 꺼지고 하트 사라져야 함
+    // 4. 지금 무조건 하트버튼 누르기만하면 데이터 누적됨 백엔드 작업해야 함(musicalId랑 userNum으로 데이터 삭제)
 
   useEffect(() => {
       const MusicalData = async () => {
@@ -63,7 +70,6 @@ const MusicalDetail = (props) => {
         try {
             const response = await MuteApi.wishReg(userNum, musicalId); // musicalId와 userNum으로 찜 상품 등록
             setWish(response.data);
-            // setModalWishReg(true);
         }
      catch (e) {
         console.log(e);
@@ -85,11 +91,11 @@ const MusicalDetail = (props) => {
         {/* wish 등록 */}
         <div onClick={() => OnClickWish()}>
         {/* <img src={heartIcon2} alt={heartIcon} width="30px"></img> */}
-            <div className={(wish ? "likeBtn" : "notLikeBtn")}>
+            {/* <div className={(wish ? "likeBtn" : "notLikeBtn")}> */}
             <p className="wish"><img src={wish ? heartIcon : heartIcon2} alt={heartIcon} width="30px"></img></p>
             </div>
 
-        </div>
+        {/* </div> */}
         
         {musicalDetail && musicalDetail.map(e => (        
             <div onClick={() => OnClickPoster(e.musicalId) }>
@@ -116,7 +122,7 @@ const MusicalDetail = (props) => {
 </div>
 </IsLikeBtn> */}
         {modalWishReg && <Modal open={modalWishReg} close={closeModalWishReg} header="&nbsp;">뮤지컬 찜 완료</Modal>}
-        {modalWishCancel && <Modal open={modalWishCancel} close={closeModalWishCancel} header='취소'>찜하기를 취소하시겠습니까?</Modal>}
+        {modalWishCancel && <Modal open={modalWishCancel} confirm = {closeModalWishCancelY} close={closeModalWishCancelN} header='취소'>찜하기를 취소하시겠습니까?</Modal>}
         {modalNotLogin && <Modal open={modalNotLogin} close={closeModalNotLogin} header="&nbsp;">로그인 후 이용하시기 바랍니다.</Modal>}
       
       </>
