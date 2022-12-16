0f57c523fa2f52ec8b4406095ed9389ccb6d6bde
silverbell <fds2236@naver.com>
2022-12-16 12:33:55 +0900

    은종/12.15/alarm

--- src/main/frontend/src/util/Alarm.js
+++ src/main/frontend/src/util/Alarm.js
@@ -4,11 +4,10 @@ import MuteApi from "../api/MuteApi";
 
 const Alarm = (props) => {
     let navigate = useNavigate();
-
-    // 로그인한 userNum을 보내서 데이터 가져오기
     const [alarm, setAlarm] = useState("");
-    const [alarmUpdate, setAlarmUpdate] = useState("");
+    const [alarmUpdate, setAlarmUpdate] = useState(0);
     let userNum =  window.localStorage.getItem("whoLoginUserNum");
+
     
     useEffect(() => {
         const AlarmInfo = async () => {
@@ -23,25 +22,28 @@ const Alarm = (props) => {
         AlarmInfo();
     }, [alarmUpdate]);
 
+
+    // 하고싶은 것 => OnClickAlarmOff할 때마다 알림 모달에서 지우고 싶음
+    // 현재 문제점 => OnClickAlarm이 실행될 때마다 OnClickAlarmOff도 같이 실행됨
+    // => 내가 OnClickAlarmOff를 클릭할 때마다 실행되어야 함
+
+    // alarmUpdate => 화면에서 하나만 사라짐
+    // setAlarmUpdate => 알림 아이콘 클릭시 무한랜더링
+    // OnClickAlarmOff => 화면에서 사라지지 않음
+
     const OnClickAlarm = (musicalId) => {
         window.localStorage.setItem("musicalId", musicalId)
-        console.log("1. 알람페이지 뮤지컬id   : " + musicalId)
-        // navigate("/MusicalDetail")
+        console.log("1. 알람아이콘 클릭 뮤지컬id   : " + musicalId)
     }
-
-    // const OnClickPoster = (musicalId) => {
-    //     window.localStorage.setItem("musicalId", musicalId)
-    //     console.log("뮤지컬id : " + musicalId)
-    //     navigate("/MusicalDetail")
-    // }
 
     const OnClickAlarmOff = (userNum, musicalId) => {
         const AlarmUpdate = async () => {
             try{
-                console.log("2. 알람 수정할 userNum :   " + userNum);
-                console.log("3. 알람 수정할 musicalId :  " + musicalId);
+                console.log("2. 알람 업데이트 전 userNum :   " + userNum);
+                console.log("3. 알람 업데이트 전 musicalId :  " + musicalId);
                 let response = await MuteApi.alarmOff(userNum, musicalId);
-                setAlarmUpdate(response.data);
+                console.log("4. 알람 업데이트 이후 : ", response.data); // 여기까지 잘 들어왔는데 재렌더링이 안됨 why?
+                setAlarmUpdate(alarmUpdate+1); // 여기서 똑같이 true라고 들어와서 값이 바뀌었다고 인식하지 못함
             } catch (e) {
                 console.log(e + "알림off 실패")
             }
@@ -50,17 +52,19 @@ const Alarm = (props) => {
     }
 
     return (
-        <>
-        {alarm && alarm.map(e => (
-        <div onClick={() => OnClickAlarm(e.musicalId) }>
-        {/* <img src= {e.musicalPoster} /> */}
-        <p>공연 이름 : {e.musicalName}</p> 
-        <p>공연 예매 시작일 : {e.musicalTicketStart}</p> 
-        <p>알림 설정 상태 : {e.alarm}</p> 
-        <button onClick={() => OnClickAlarmOff(e.userNum, e.musicalId)}>OFF</button>
+        <div className="alarm_container">
+            {alarm && alarm.map(e => (
+            <div className="alarm">
+                <div onClick={() => OnClickAlarm(e.musicalId) }>
+                <p>공연 이름 : {e.musicalName}</p> 
+                <p>공연 예매 시작일 : {e.musicalTicketStart}</p> 
+                <p>알림 설정 상태 : {e.alarm}</p> 
+                </div>
+                <button onClick={() => OnClickAlarmOff(e.userNum, e.musicalId)}>OFF</button>
+            </div>
+            ))}
+
         </div>
-        ))}
-        </>
         
     )
 }
